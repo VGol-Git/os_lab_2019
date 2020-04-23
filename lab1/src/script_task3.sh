d398fafa3b22526bfe02029b0636283e995db863
vgol-git <gidracomp@yandex.ru>
2020-04-23 21:47:51 +0000

    Добавление скрипта к заданию 3

---
+++ lab1/src/script_task3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo
+echo "Текущий путь:"
+realpath script_task3
+echo
+echo "Текущая дата и время:"
+date
+echo
+echo "Содержимое PATH:"
+echo $PATH
+echo
+Добавление скрина к заданию 3
