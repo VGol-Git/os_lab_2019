13860023acc65d10fffcc4c7a5d904371cc1c3cc
vgol-git <gidracomp@yandex.ru>
2020-04-25 07:07:58 +0000

    Выполнено задание 2

--- lab2/src/revert_string/main.c
+++ lab2/src/revert_string/main.c
@@ -6,19 +6,21 @@
 
 int main(int argc, char *argv[])
 {
+    //Проверка числа аргуметов командной строки (включая имя программы)
 	if (argc != 2)
 	{
-		printf("Usage: %s string_to_revert\n", argv[0]);
-		return -1;
+		printf("Usage: %s string_to_revert\n", argv[0]);  //Печать имени файла
+       
+		return -1;//Завершение работы программы
 	}
 
-	char *reverted_str = malloc(sizeof(char) * (strlen(argv[1]) + 1));
-	strcpy(reverted_str, argv[1]);
+	char *reverted_str = malloc(sizeof(char) * (strlen(argv[1]) + 1)); // выдеоение памяти размером "число символов в строке" + 1)
+	strcpy(reverted_str, argv[1]); //крпмрование строки
 
-	RevertString(reverted_str);
+	RevertString(reverted_str);//вызов функции, которая "переворачивает" слово
 
-	printf("Reverted: %s\n", reverted_str);
-	free(reverted_str);
+	printf("Reverted: %s\n", reverted_str); //Печать результирующей строки
+	free(reverted_str);//очищение памяти
 	return 0;
 }
 
--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,14 @@
 #include "revert_string.h"
-
+#include <string.h>
 void RevertString(char *str)
 {
-	// your code here
+    //int size_char = strlen(str);
+    int i;
+    for (i=0; i < strlen(str) / 2; i++)
+    {
+        char buf = str[i];
+        str[i] = str[strlen(str)-i-1];
+        str[strlen(str)-i-1]=buf;
+    }
 }
 
