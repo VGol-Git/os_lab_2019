e015a91be91c530f2a2b2dccc2559a30357c6c8c
vgol-git <gidracomp@yandex.ru>
2020-06-18 12:06:26 +0000

    task1

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -6,7 +6,14 @@ struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
   struct MinMax min_max;
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
-
-  // your code here
+  for (int i = 0; i < end; ++i){
+    if (array[i] > min_max.max){
+      min_max.max = array[i];
+      continue;
+    }
+    if (array[i] < min_max.min)
+      min_max.min = array[i];  
+  }
+  
   return min_max;
 }
--- lab3/src/sequential_min_max.c
+++ lab3/src/sequential_min_max.c
@@ -5,28 +5,32 @@
 #include "utils.h"
 
 int main(int argc, char **argv) {
+//проверка кол-ва аргументов 
   if (argc != 3) {
     printf("Usage: %s seed arraysize\n", argv[0]);
     return 1;
   }
-
+//проверка для генерации пвседослучайного числа > 0
   int seed = atoi(argv[1]);
   if (seed <= 0) {
     printf("seed is a positive number\n");
     return 1;
   }
-
+//проверка длины массива 
   int array_size = atoi(argv[2]);
   if (array_size <= 0) {
     printf("array_size is a positive number\n");
     return 1;
   }
-
+//выделение памяти для массива
   int *array = malloc(array_size * sizeof(int));
+//заполнение массива псевдослучайными числами 
   GenerateArray(array, array_size, seed);
+//поиск min и max
   struct MinMax min_max = GetMinMax(array, 0, array_size);
+//освобождение памяти 
   free(array);
-
+//вывод результата
   printf("min: %d\n", min_max.min);
   printf("max: %d\n", min_max.max);
 
